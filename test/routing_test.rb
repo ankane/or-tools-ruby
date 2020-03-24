521067f074a278de9429e8c632835ea0eecd5bad
Andrew Kane <andrew@chartkick.com>
2020-03-24 00:16:29 -0700

    Started pickup and delivery

--- test/routing_test.rb
+++ test/routing_test.rb
@@ -213,6 +213,95 @@ class RoutingTest < Minitest::Test
     puts "Total load of all routes: #{total_load}"
   end
 
+  def test_pickup_delivery
+    data = {}
+    data[:distance_matrix] = [
+      [0, 548, 776, 696, 582, 274, 502, 194, 308, 194, 536, 502, 388, 354, 468, 776, 662],
+      [548, 0, 684, 308, 194, 502, 730, 354, 696, 742, 1084, 594, 480, 674, 1016, 868, 1210],
+      [776, 684, 0, 992, 878, 502, 274, 810, 468, 742, 400, 1278, 1164, 1130, 788, 1552, 754],
+      [696, 308, 992, 0, 114, 650, 878, 502, 844, 890, 1232, 514, 628, 822, 1164, 560, 1358],
+      [582, 194, 878, 114, 0, 536, 764, 388, 730, 776, 1118, 400, 514, 708, 1050, 674, 1244],
+      [274, 502, 502, 650, 536, 0, 228, 308, 194, 240, 582, 776, 662, 628, 514, 1050, 708],
+      [502, 730, 274, 878, 764, 228, 0, 536, 194, 468, 354, 1004, 890, 856, 514, 1278, 480],
+      [194, 354, 810, 502, 388, 308, 536, 0, 342, 388, 730, 468, 354, 320, 662, 742, 856],
+      [308, 696, 468, 844, 730, 194, 194, 342, 0, 274, 388, 810, 696, 662, 320, 1084, 514],
+      [194, 742, 742, 890, 776, 240, 468, 388, 274, 0, 342, 536, 422, 388, 274, 810, 468],
+      [536, 1084, 400, 1232, 1118, 582, 354, 730, 388, 342, 0, 878, 764, 730, 388, 1152, 354],
+      [502, 594, 1278, 514, 400, 776, 1004, 468, 810, 536, 878, 0, 114, 308, 650, 274, 844],
+      [388, 480, 1164, 628, 514, 662, 890, 354, 696, 422, 764, 114, 0, 194, 536, 388, 730],
+      [354, 674, 1130, 822, 708, 628, 856, 320, 662, 388, 730, 308, 194, 0, 342, 422, 536],
+      [468, 1016, 788, 1164, 1050, 514, 514, 662, 320, 274, 388, 650, 536, 342, 0, 764, 194],
+      [776, 868, 1552, 560, 674, 1050, 1278, 742, 1084, 810, 1152, 274, 388, 422, 764, 0, 798],
+      [662, 1210, 754, 1358, 1244, 708, 480, 856, 514, 468, 354, 844, 730, 536, 194, 798, 0]
+    ]
+    data[:pickups_deliveries] = [
+      [1, 6],
+      [2, 10],
+      [4, 3],
+      [5, 9],
+      [7, 8],
+      [15, 11],
+      [13, 12],
+      [16, 14],
+    ]
+    data[:num_vehicles] = 4
+    data[:depot] = 0
+
+    manager = ORTools::RoutingIndexManager.new(data[:distance_matrix].size, data[:num_vehicles], data[:depot])
+
+    routing = ORTools::RoutingModel.new(manager)
+
+    distance_callback = lambda do |from_index, to_index|
+      from_node = manager.index_to_node(from_index)
+      to_node = manager.index_to_node(to_index)
+      data[:distance_matrix][from_node][to_node]
+    end
+
+    transit_callback_index = routing.register_transit_callback(distance_callback)
+    routing.set_arc_cost_evaluator_of_all_vehicles(transit_callback_index)
+
+    dimension_name = "Distance"
+    routing.add_dimension(
+      transit_callback_index,
+      0,  # no slack
+      3000,  # vehicle maximum travel distance
+      true,  # start cumul to zero
+      dimension_name
+    )
+    distance_dimension = routing.mutable_dimension(dimension_name)
+    distance_dimension.global_span_cost_coefficient = 100
+
+    skip "Not finished yet"
+
+    data[:pickups_deliveries].each do |request|
+      pickup_index = manager.node_to_index(request[0])
+      delivery_index = manager.node_to_index(request[1])
+      routing.add_pickup_and_delivery(pickup_index, delivery_index)
+      routing.solver.add(routing.vehicle_var(pickup_index) == routing.vehicle_var(delivery_index))
+      routing.solver.add(distance_dimension.cumul_var(pickup_index) <= distance_dimension.cumul_var(delivery_index))
+    end
+
+    solution = routing.solve(first_solution_strategy: :parallel_cheapest_insertion)
+
+    total_distance = 0
+    data[:num_vehicles].times do |vehicle_id|
+      index = routing.start(vehicle_id)
+      plan_output = String.new("Route for vehicle #{vehicle_id}:\n")
+      route_distance = 0
+      while !routing.end?(index)
+        plan_output += " #{manager.index_to_node(index)} -> "
+        previous_index = index
+        index = solution.value(routing.next_var(index))
+        route_distance += routing.arc_cost_for_vehicle(previous_index, index, vehicle_id)
+      end
+      plan_output += "#{manager.index_to_node(index)}\n"
+      plan_output += "Distance of the route: #{route_distance}m\n\n"
+      puts plan_output
+      total_distance += route_distance
+    end
+    puts "Total Distance of all routes: #{total_distance}m"
+  end
+
   def test_vrptw
     data = {}
     data[:time_matrix] = [
