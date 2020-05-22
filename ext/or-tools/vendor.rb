3010c8e03626711320c6a132ea5bd84b848a559f
Andrew Kane <andrew@chartkick.com>
2020-05-21 20:21:18 -0700

    Less space [skip ci]

--- ext/or-tools/vendor.rb
+++ ext/or-tools/vendor.rb
@@ -25,11 +25,9 @@ else
     checksum = "a2b800d4e498561e5b1fe95ee1e64c867be496038883f4f7b199499bf71a0eed"
   else
     abort <<~MSG
-
       Binary installation not available for this platform.
       Build the OR-Tools C++ library from source, then run:
       bundle config build.or-tools --with-or-tools-dir=/path/to/or-tools
-
     MSG
   end
 end
