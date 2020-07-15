925e0801a78a472db2b00d3645a0ee7b5c5df279
Andrew Kane <andrew@chartkick.com>
2020-07-15 16:50:08 -0700

    Renamed variable [skip ci]

--- lib/or_tools/seating.rb
+++ lib/or_tools/seating.rb
@@ -5,18 +5,18 @@ module ORTools
     def initialize(connections:, tables:)
       @people = connections.flat_map { |c| c[:people] }.uniq
 
-      @connection_map = {}
+      @connection_for = {}
       @people.each do |person|
-        @connection_map[person] = {}
+        @connection_for[person] = {}
       end
       connections.each do |c|
         c[:people].each_with_index do |person, i|
           others = c[:people].dup
           others.delete_at(i)
           others.each do |other|
-            @connection_map[person][other] ||= 0
+            @connection_for[person][other] ||= 0
             # currently additive, but could use max
-            @connection_map[person][other] += c[:strength]
+            @connection_for[person][other] += c[:strength]
           end
         end
       end
@@ -25,7 +25,7 @@ module ORTools
     end
 
     def connections_for(person)
-      @connection_map[person]
+      @connection_for[person]
     end
   end
 end
