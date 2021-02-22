618a8e0afc126c0be637064e8c30cc61f95bdb37
Andrew Kane <andrew@ankane.org>
2021-02-22 15:30:12 -0800

    Improved test [skip ci]

--- test/expression_test.rb
+++ test/expression_test.rb
@@ -27,15 +27,15 @@ class ExpressionTest < Minitest::Test
 
   def test_add_max_equality
     model = ORTools::CpModel.new
-    x = model.new_int_var(0, 1, "x")
-    y = model.new_int_var(0, 2, "y")
-    model.add_max_equality(y, [x, model.new_int_var(2, 2, "z")])
+    x = model.new_int_var(-7, 7, "x")
+    y = model.new_int_var(0, 7, "y")
+    model.add_max_equality(y, [x, model.new_int_var(0, 0, "z")])
     # TODO support
     # model.add_max_equality(y, [x, 2])
 
     solver = ORTools::CpSolver.new
     assert_equal :optimal, solver.solve(model)
-    assert_equal 0, solver.value(x)
-    assert_equal 2, solver.value(y)
+    assert_equal -7, solver.value(x)
+    assert_equal 0, solver.value(y)
   end
 end
