5b168318272f7739170f63d9b180f31def667604
Andrew Kane <andrew@ankane.org>
2021-02-23 12:08:10 -0800

    Moved solution printer test to new file [skip ci]

--- test/expression_test.rb
+++ test/expression_test.rb
@@ -39,21 +39,6 @@ class ExpressionTest < Minitest::Test
     assert_equal 0, solver.value(y)
   end
 
-  def test_objective_solution_printer
-    model = ORTools::CpModel.new
-    x = model.new_int_var(-7, 7, "x")
-    y = model.new_int_var(0, 7, "y")
-    model.add_max_equality(y, [x, model.new_constant(0)])
-
-    solver = ORTools::CpSolver.new
-    solution_printer = ORTools::ObjectiveSolutionPrinter.new
-    stdout, _ = capture_io do
-      solver.search_for_all_solutions(model, solution_printer)
-    end
-    assert_match "Solution 14", stdout
-    refute_match "Solution 15", stdout
-  end
-
   def test_to_s
     model = ORTools::CpModel.new
     x = model.new_int_var(-7, 7, "x")
---
+++ test/solution_printer_test.rb
@@ -0,0 +1,18 @@
+require_relative "test_helper"
+
+class SolutionPrinterTest < Minitest::Test
+  def test_objective_solution_printer
+    model = ORTools::CpModel.new
+    x = model.new_int_var(-7, 7, "x")
+    y = model.new_int_var(0, 7, "y")
+    model.add_max_equality(y, [x, model.new_constant(0)])
+
+    solver = ORTools::CpSolver.new
+    solution_printer = ORTools::ObjectiveSolutionPrinter.new
+    stdout, _ = capture_io do
+      solver.search_for_all_solutions(model, solution_printer)
+    end
+    assert_equal 15, solution_printer.solution_count
+    assert_match "Solution 14", stdout
+  end
+end
