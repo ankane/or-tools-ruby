288828de0286a0ad11726d6f3caed6fc7e205b35
Andrew Kane <andrew@ankane.org>
2021-04-28 22:50:33 -0700

    Updated Rice

--- ext/or-tools/ext.h
+++ ext/or-tools/ext.h
@@ -2,16 +2,3 @@
 
 #include <rice/rice.hpp>
 #include <rice/stl.hpp>
-
-// TODO remove
-namespace Rice::detail
-{
-  template<>
-  struct Type<Rice::Symbol>
-  {
-    static bool verify()
-    {
-      return true;
-    }
-  };
-}
