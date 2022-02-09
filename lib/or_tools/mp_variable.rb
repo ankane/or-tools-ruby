7144bc55431401e2706aff5ef12d732e6f82ac59
Andrew Kane <andrew@ankane.org>
2022-02-08 19:19:34 -0800

    Updated todo [skip ci]

--- lib/or_tools/mp_variable.rb
+++ lib/or_tools/mp_variable.rb
@@ -1,5 +1,5 @@
 module ORTools
-  # TODO change to Variable in 0.7.0
+  # TODO change to VariableExpr in 0.7.0
   class MPVariable < LinearExpr
     def add_self_to_coeff_map_or_stack(coeffs, multiplier, stack)
       coeffs[self] += multiplier
