2c84ccf4a8db27bc1750b38ebdb015c86e398044
Andrew Kane <andrew@ankane.org>
2022-05-27 16:25:39 -0700

    Moved test [skip ci]

--- test/assignment_test.rb
+++ test/assignment_test.rb
@@ -1,34 +1,6 @@
 require_relative "test_helper"
 
 class AssignmentTest < Minitest::Test
-  # https://developers.google.com/optimization/assignment/linear_assignment
-  def test_linear_sum_assignment
-    cost = [[ 90,  76, 75,  70],
-            [ 35,  85, 55,  65],
-            [125,  95, 90, 105],
-            [ 45, 110, 95, 115]]
-
-    rows = cost.length
-    cols = cost[0].length
-
-    assignment = ORTools::LinearSumAssignment.new
-    rows.times do |worker|
-      cols.times do |task|
-        if cost[worker][task]
-          assignment.add_arc_with_cost(worker, task, cost[worker][task])
-        end
-      end
-    end
-
-    assert_equal :optimal, assignment.solve
-    assert_equal 265, assignment.optimal_cost
-    assert_equal 4, assignment.num_nodes
-
-    nodes = assignment.num_nodes.times
-    assert_equal [3, 2, 1, 0], nodes.map { |i| assignment.right_mate(i) }
-    assert_equal [70, 55, 95, 45], nodes.map { |i| assignment.assignment_cost(i) }
-  end
-
   # https://developers.google.com/optimization/assignment/assignment_teams#mip
   def test_assignment_teams
     solver = ORTools::Solver.create("CBC")
@@ -85,5 +57,33 @@ class AssignmentTest < Minitest::Test
     end
 
     assert_equal [[0, 2], [1, 0], [4, 3], [5, 1]], assignments
+  end
+
+  # https://developers.google.com/optimization/assignment/linear_assignment
+  def test_linear_sum_assignment
+    cost = [[ 90,  76, 75,  70],
+            [ 35,  85, 55,  65],
+            [125,  95, 90, 105],
+            [ 45, 110, 95, 115]]
+
+    rows = cost.length
+    cols = cost[0].length
+
+    assignment = ORTools::LinearSumAssignment.new
+    rows.times do |worker|
+      cols.times do |task|
+        if cost[worker][task]
+          assignment.add_arc_with_cost(worker, task, cost[worker][task])
+        end
+      end
+    end
+
+    assert_equal :optimal, assignment.solve
+    assert_equal 265, assignment.optimal_cost
+    assert_equal 4, assignment.num_nodes
+
+    nodes = assignment.num_nodes.times
+    assert_equal [3, 2, 1, 0], nodes.map { |i| assignment.right_mate(i) }
+    assert_equal [70, 55, 95, 45], nodes.map { |i| assignment.assignment_cost(i) }
   end
 end
