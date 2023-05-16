9dae425e961c2314874940721e36166a95813916
Andrew Kane <andrew@ankane.org>
2023-05-15 22:56:58 -0700

    Improved callback code

--- ext/or-tools/constraint.cpp
+++ ext/or-tools/constraint.cpp
@@ -409,9 +409,12 @@ void init_constraint(Rice::Module& m) {
 
           m.Add(NewFeasibleSolutionObserver(
             [&callback](const CpSolverResponse& r) {
-              // TODO find a better way to do this
-              callback.call("response=", r);
-              callback.call("on_solution_callback");
+              // ensure Ruby thread
+              if (ruby_native_thread_p()) {
+                // TODO find a better way to do this
+                callback.iv_set("@response", r);
+                callback.call("on_solution_callback");
+              }
             })
           );
         }
