27b9bb1dd5db475b311fedfd1242df917867e7b4
Andrew Kane <andrew@ankane.org>
2023-05-15 23:21:40 -0700

    Debug

--- ext/or-tools/constraint.cpp
+++ ext/or-tools/constraint.cpp
@@ -416,12 +416,12 @@ void init_constraint(Rice::Module& m) {
           m.Add(NewFeasibleSolutionObserver(
             [&callback](const CpSolverResponse& r) {
               // ensure Ruby thread
-              if (ruby_native_thread_p()) {
-                // TODO find a better way to do this
-                callback.iv_set("@response", r);
-                callback.call("on_solution_callback");
-                // stopped = callback.attr_get("@stopped").test();
-              }
+              // if (ruby_native_thread_p()) {
+              //   // TODO find a better way to do this
+              //   callback.iv_set("@response", r);
+              //   callback.call("on_solution_callback");
+              //   // stopped = callback.attr_get("@stopped").test();
+              // }
             })
           );
         }
