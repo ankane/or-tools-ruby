1db728e5dff14dea21b37b25480083d5756d1750
Andrew Kane <andrew@ankane.org>
2024-10-06 11:53:11 -0700

    Improved requires [skip ci]

--- lib/or-tools.rb
+++ lib/or-tools.rb
@@ -1,27 +1,34 @@
 # ext
 require "or_tools/ext"
 
-# modules
+# expressions
 require_relative "or_tools/comparison"
 require_relative "or_tools/comparison_operators"
+
+# bin packing
+require_relative "or_tools/knapsack_solver"
+
+# constraint
 require_relative "or_tools/bool_var"
-require_relative "or_tools/linear_expr"
-require_relative "or_tools/constant"
 require_relative "or_tools/cp_model"
 require_relative "or_tools/cp_solver"
 require_relative "or_tools/cp_solver_solution_callback"
-require_relative "or_tools/int_var"
-require_relative "or_tools/knapsack_solver"
-require_relative "or_tools/linear_constraint"
-require_relative "or_tools/mp_variable"
-require_relative "or_tools/product_cst"
-require_relative "or_tools/routing_index_manager"
-require_relative "or_tools/routing_model"
-require_relative "or_tools/sat_linear_expr"
 require_relative "or_tools/sat_int_var"
+require_relative "or_tools/sat_linear_expr"
+
+# linear
+require_relative "or_tools/linear_expr"
+require_relative "or_tools/constant"
+require_relative "or_tools/mp_variable"
+require_relative "or_tools/linear_constraint"
+require_relative "or_tools/product_cst"
 require_relative "or_tools/solver"
 require_relative "or_tools/sum_array"
-require_relative "or_tools/version"
+
+# routing
+require_relative "or_tools/int_var"
+require_relative "or_tools/routing_index_manager"
+require_relative "or_tools/routing_model"
 
 # solution printers
 require_relative "or_tools/objective_solution_printer"
@@ -33,6 +40,9 @@ require_relative "or_tools/basic_scheduler"
 require_relative "or_tools/seating"
 require_relative "or_tools/sudoku"
 require_relative "or_tools/tsp"
+
+# modules
+require_relative "or_tools/version"
 
 module ORTools
   class Error < StandardError; end
