4485ede0ff93bde82cc184a7bba70c204e094d9e
Andrew Kane <andrew@ankane.org>
2024-10-06 14:45:38 -0700

    Removed todo [skip ci]

--- test/constraint_test.rb
+++ test/constraint_test.rb
@@ -242,8 +242,7 @@ class ConstraintTest < Minitest::Test
     x = model.new_int_var(lower_bound, upper_bound, "x")
 
     assert_equal lower_bound, x.domain.min
-    # TODO figure out segfault on Mac ARM
-    # assert_equal upper_bound, x.domain.max
+    assert_equal upper_bound, x.domain.max
   end
 
   def test_sum_empty_true
